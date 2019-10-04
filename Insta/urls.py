d8ff6e501ddbe74d319d7a8f83e2c515947cb577
Daniel Bu <danielbu@Daniel-Bus-MBP.local>
2019-10-03 23:58:00 -0400

    Add CRUD

--- Insta/urls.py
+++ Insta/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 
-from . import views
+from Insta.views import *
 
 urlpatterns = [
-    path('', views.HelloWorld.as_view(), name='helloworld'),
+    path('', HelloWorld.as_view(), name='helloworld'),
+    path('posts/', PostsView.as_view(), name='posts'),
+    path('post/<int:pk>', PostDetailView.as_view(), name='post_detail'),
+    path('post/new/', PostCreateView.as_view(), name='post_create'),
+    path('post/update/<int:pk>', PostUpdateView.as_view(), name='post_update'),
+    path('post/delete/<int:pk>', PostDeleteView.as_view(), name='post_delete'),
 ]
