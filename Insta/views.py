3694817f8adb2f8f84b35f36cbf5097e5d76575e
Daniel Bu <danielbu@Daniel-Bus-MBP.local>
2019-10-09 00:34:42 -0400

    Add auth

--- Insta/views.py
+++ Insta/views.py
@@ -2,7 +2,8 @@ from django.views.generic import TemplateView, ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from Insta.models import *
-
+from Insta.forms import CustomUserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
 class HelloWorld(TemplateView):
     template_name = 'helloworld.html'
@@ -15,10 +16,11 @@ class PostDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     template_name = 'post_create.html'
     fields = '__all__'
+    login_url = 'login'
 
 class PostUpdateView(UpdateView):
     model = Post
@@ -29,3 +31,8 @@ class PostDeleteView(DeleteView):
     model = Post
     template_name = 'post_delete.html'
     success_url = reverse_lazy("posts")
+
+class Signup(CreateView):
+    form_class = CustomUserCreationForm
+    template_name = 'signup.html'
+    success_url = reverse_lazy("login")
